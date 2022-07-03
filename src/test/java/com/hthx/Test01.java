21b1a7702b769132101a5f4326eaaa735a619d42
ZWD <490857147@qq.com>
2022-07-03 21:44:24 +0800

    第一个demo

--- src/test/java/com/hthx/Test01.java
+++ src/test/java/com/hthx/Test01.java
@@ -1,13 +1,15 @@
-package com.hthx.controller;
+package com.hthx;
 
+import com.hthx.controller.Logging;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Logging {
+public class Test01 {
 
-    public static final Logger LOGGER = LoggerFactory.getLogger(Logging.class);
-
-    public static void main(String[] args) {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Test01.class);
+    @Test
+    public void test01(){
         LOGGER.error("error");
         LOGGER.warn("warn");
         LOGGER.info("info");
